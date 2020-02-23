f18fbaa65ad9deafc2ee8978bdb40018bd3c631a
Basilio Bazan <basilio@cs.utexas.edu>
2020-02-23 15:23:03 -0600

    Raycasting Enemies

--- Project-TeamTrinity/Assets/Scripts/EnemyController.cs
+++ Project-TeamTrinity/Assets/Scripts/EnemyController.cs
@@ -5,9 +5,31 @@ using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    public GameObject[] waypoints;
     private NavMeshAgent myAgent;
+    //public Animator anim;
+
+    public enum State
+    {
+        PATROL,
+        CHASE,
+        SEARCH
+    }
+    public State state;
+    private bool alive;
+
+    public GameObject[] waypoints;
     private int currentWaypoint;
+    public float patrolSpeed = 3f;
+
+    public float chaseSpeed = 5f;
+    public GameObject player;
+
+    private Vector3 searchSpot;
+    private float timer;
+    public float searchWait = 10;
+
+    private float heightMultiplyer;
+    private float sightDistance = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -15,11 +37,42 @@ public class EnemyController : MonoBehaviour
         currentWaypoint = 0;
         myAgent = GetComponent<NavMeshAgent>();
         myAgent.destination = waypoints[currentWaypoint].transform.position;
+
+        myAgent.updatePosition = true;
+        myAgent.updateRotation = true;
+
+        state = EnemyController.State.PATROL;
+        alive = true;
+
+        heightMultiplyer = 0.8f;
+
+        StartCoroutine("EnemyStates");
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator EnemyStates()
     {
+        while (alive)
+        {
+            switch (state)
+            {
+                case State.PATROL:
+                    Patrol();
+                    break;
+                case State.CHASE:
+                    Chase();
+                    break;
+                case State.SEARCH:
+                    Search();
+                    break;
+            }
+            yield return null;
+        }
+    }
+
+    void Patrol()
+    {
+        Debug.Log("PATROL");
+        myAgent.speed = patrolSpeed;
         //If enemy has reached waypoint
         if (Vector3.Distance(myAgent.destination, transform.position) <= 2)
         {
@@ -33,6 +86,81 @@ public class EnemyController : MonoBehaviour
             }
             //enemy destination set to current waypoint
             myAgent.destination = waypoints[currentWaypoint].transform.position;
+        }
+    }
+
+    void Chase()
+    {
+        Debug.Log("CHASE");
+        myAgent.speed = chaseSpeed;
+        myAgent.SetDestination(player.transform.position);
+        if (Vector3.Distance(player.transform.position, transform.position) > 10)
+        {
+            searchSpot = player.transform.position;
+            state = EnemyController.State.SEARCH;
+        }
+    }
+
+    void Search()
+    {
+        Debug.Log("SEARCH");
+        timer += Time.deltaTime;
+        myAgent.SetDestination(this.transform.position);
+        transform.LookAt(searchSpot);
+        if(timer >= searchWait)
+        {
+            state = EnemyController.State.PATROL;
+            timer = 0;
+        }
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.tag == "Player")
+    //    {
+    //        state = EnemyController.State.CHASE;
+    //        player = other.gameObject;
+    //    }
+    //}
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        //Debug lines to show sight cone and detection height
+        Debug.DrawRay(transform.position + Vector3.up * heightMultiplyer, transform.forward * sightDistance, Color.green);
+        Debug.DrawRay(transform.position + Vector3.up * heightMultiplyer, (transform.forward + transform.right).normalized * sightDistance, Color.green);
+        Debug.DrawRay(transform.position + Vector3.up * heightMultiplyer, (transform.forward - transform.right).normalized * sightDistance, Color.green);
+
+        //Raycsat Straight ahead
+        if (Physics.Raycast(transform.position + Vector3.up * heightMultiplyer, transform.forward, out hit, sightDistance))
+        {
+            if (hit.collider.gameObject.tag == "Player")
+            {
+                Debug.Log("DETECTED!");
+                player = hit.collider.gameObject;
+                state = EnemyController.State.CHASE;
+            }
+        }
+        //Raycast right edge of sight cone
+        if (Physics.Raycast(transform.position + Vector3.up * heightMultiplyer, (transform.forward + transform.right).normalized, out hit, sightDistance))
+        {
+            if (hit.collider.gameObject.tag == "Player")
+            {
+                Debug.Log("DETECTED!");
+                player = hit.collider.gameObject;
+                state = EnemyController.State.CHASE;
+            }
+        }
+        //Raycast left edge of sight cone
+        if (Physics.Raycast(transform.position + Vector3.up * heightMultiplyer, (transform.forward - transform.right).normalized, out hit, sightDistance))
+        {
+            if (hit.collider.gameObject.tag == "Player")
+            {
+                Debug.Log("DETECTED!");
+                player = hit.collider.gameObject;
+                state = EnemyController.State.CHASE;
+            }
         }
     }
 }
