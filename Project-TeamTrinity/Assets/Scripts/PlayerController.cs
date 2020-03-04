b8ad6830d692cce687635dbd1ac9c88589dadd70
Basilio Bazan <basilio@cs.utexas.edu>
2020-03-04 12:56:43 -0600

    Crouching Works

--- Project-TeamTrinity/Assets/Scripts/PlayerController.cs
+++ Project-TeamTrinity/Assets/Scripts/PlayerController.cs
@@ -8,16 +8,37 @@ public class PlayerController : MonoBehaviour
     public GameObject GameOverScreen;
     public UnityStandardAssets.Characters.FirstPerson.FirstPersonController controller;
 
+    public Transform cameraTransform;
+    public Transform colliderTransform;
+    private Vector3 crouchAdjust;
+
+    public bool isCrouching = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        crouchAdjust = new Vector3(0, 0.5f, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("left ctrl"))
+        {
+            Debug.Log("CROUCHING");
+            isCrouching = true;
+            cameraTransform.position -= crouchAdjust;
+            colliderTransform.position -= crouchAdjust;
+            controller.m_WalkSpeed = 3;
+        }
+        else if(isCrouching == true && Input.GetKeyUp("left ctrl"))
+        {
+            Debug.Log("NOT CROUCHING");
+            isCrouching = false;
+            cameraTransform.position += crouchAdjust;
+            colliderTransform.position += crouchAdjust;
+            controller.m_WalkSpeed = 5;
+        }
     }
 
     public void OnTriggerEnter(Collider other)
--- Project-TeamTrinity/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Project-TeamTrinity/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -11,7 +11,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
     public class FirstPersonController : MonoBehaviour
     {
         [SerializeField] private bool m_IsWalking;
-        [SerializeField] private float m_WalkSpeed;
+        public float m_WalkSpeed;
         [SerializeField] private float m_RunSpeed;
         [SerializeField] [Range(0f, 1f)] private float m_RunstepLenghten;
         [SerializeField] private float m_JumpSpeed;
@@ -28,7 +28,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         [SerializeField] private AudioClip m_JumpSound;           // the sound played when character leaves the ground.
         [SerializeField] private AudioClip m_LandSound;           // the sound played when character touches back on ground.
 
-        private Camera m_Camera;
+        public Camera m_Camera;
         private bool m_Jump;
         private float m_YRotation;
         private Vector2 m_Input;
@@ -128,7 +128,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
             m_CollisionFlags = m_CharacterController.Move(m_MoveDir*Time.fixedDeltaTime);
 
             ProgressStepCycle(speed);
-            UpdateCameraPosition(speed);
+            //UpdateCameraPosition(speed);
 
             //m_MouseLook.UpdateCursorLock();
         }
